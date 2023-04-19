fd34080cd044c7fb75e852922ef56ae1f07092b5
Rahul <rahul.unnikrishnan.nair@intel.com>
2023-04-19 12:52:02 -0700

    Update env_dev_utils_setup.sh

--- base/env_dev_utils_setup.sh
+++ base/env_dev_utils_setup.sh
@@ -65,7 +65,7 @@ sudo apt-get install -y \
 
 # install and setup Docker
 colored_output "Adding Docker repository and installing Docker..." blue
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor | sudo sh -c "cat > /usr/share/keyrings/docker-archive-keyring.gpg"
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
