44440c783fe7649c27b75900fdbc0b5e4352a6b0
Rahul Nair <rahulunair@users.noreply.github.com>
2023-04-21 15:11:10 -0700

    Update 2_kernel_setup.sh

--- base/2_kernel_setup.sh
+++ base/2_kernel_setup.sh
@@ -3,8 +3,8 @@
 export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_FRONTEND=noninteractive
 export NEEDRESTART_MODE=a
-KERNEL_NAME="linux-image-5.17.0-35-generic"
-KERNEL_VERSION="5.17.0-35"
+KERNEL_NAME="linux-image-5.15.0-57-generic"
+KERNEL_VERSION="5.15.0-57"
 
 alias sudo="sudo -E"
 colored_output() {
