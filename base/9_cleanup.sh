8df8820de5e2bd321c1425da99dc1c027605dd7a
Rahul Nair <rahulunair@users.noreply.github.com>
2023-04-24 14:48:40 -0700

    Update 9_cleanup.sh

--- base/9_cleanup.sh
+++ base/9_cleanup.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
-#
+
+set -e
+export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_FRONTEND=noninteractive
 export NEEDRESTART_MODE=a
+KERNEL_NAME="linux-image-5.15.0-57-generic"
+KERNEL_VERSION="5.15.0-57"
+
 alias sudo="sudo -E"
-set -e
+colored_output() {
+    local text="$1"
+    local color="$2"
+
+    case "${color}" in
+        red)    echo -e "\033[31m${text}\033[0m" ;;
+        green)  echo -e "\033[32m${text}\033[0m" ;;
+        yellow) echo -e "\033[33m${text}\033[0m" ;;
+        blue)   echo -e "\033[34m${text}\033[0m" ;;
+        *)      echo "${text}" ;;
+    esac
+}
 
 sudo apt update &&\
   sudo apt upgrade -y &&\
   sudo apt autoremove
+
+# Reboot
+colored_output "Rebooting the system in 10 seconds..." blue
+sleep 10
+sudo reboot
