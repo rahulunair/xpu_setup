b3e2e2664a21110ec4cb6beafd1853adb7249344
Rahul Nair <rahulunair@users.noreply.github.com>
2023-04-24 11:18:07 -0700

    Update 2_kernel_setup.sh

--- base/2_kernel_setup.sh
+++ base/2_kernel_setup.sh
@@ -33,8 +33,8 @@ else
 fi
 
 colored_output "Setting kernel ${KERNEL_VERSION}-generic as the default kernel..." blue
-sudo sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=\"1> $(echo $(($(awk -F\' '/menuentry / {print $2}' /boot/grub/grub.cfg \
-| grep -no '${KERNEL_VERSION}' | sed 's/:/\n/g' | head -n 1)-2)))\"/" /etc/default/grub
+entry=$(echo $(awk -F\' '/menuentry / {print $2}' /boot/grub/grub.cfg | grep "${KERNEL_VERSION}" | head -n 1))
+sudo sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=\"Advanced options for Ubuntu>${entry}\"/g" /etc/default/grub
 sudo sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"$(echo $(awk -F'="' '$1  == "GRUB_CMDLINE_LINUX_DEFAULT" {print $2}'  \
 /etc/default/grub | tr -d '"') | sed 's/pci=realloc=off//g') pci=realloc=off\"/" /etc/default/grub
 
