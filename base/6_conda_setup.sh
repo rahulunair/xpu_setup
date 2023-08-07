7a140ad5d0058bde5b4a3ddf393eae7f3e0b6cbd
Rahul <rahul.unnikrishnan.nair@intel.com>
2023-08-07 16:30:49 -0700

    Update 6_conda_setup.sh

--- base/6_conda_setup.sh
+++ base/6_conda_setup.sh
@@ -64,7 +64,7 @@ install_ai_packages() {
         elif [[ "$env" == "tensorflow_xpu" ]] ; then
             sudo $CONDA_PATH run -n $env python -m pip install --upgrade intel-extension-for-tensorflow[gpu]
         elif [[ "$env" == "pytorch_xpu" ]] ; then
-            sudo $CONDA_PATH run -n $env python -m pip install torch==1.13.0a0+git6c9b55e intel_extension_for_pytorch==1.13.120+xpu -f https://developer.intel.com/ipex-whl-stable-xpu
+            sudo $CONDA_PATH run -n $env python -m pip install torch==2.0.1a0 torchvision==0.15.2a0 intel_extension_for_pytorch==2.0.110+xpu -f https://developer.intel.com/ipex-whl-stable-xpu
         fi
     done
 }
